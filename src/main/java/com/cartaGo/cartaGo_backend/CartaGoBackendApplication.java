6a6651e085a9e5a7601268b32b17164b26995eba
alba-rg-14 <albaruizgutierrez@uma.es>
2025-05-19 19:20:42 +0200

    Primer commit del backend CartaGo

---
+++ src/main/java/com/cartaGo/cartaGo_backend/CartaGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cartaGo.cartaGo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaGoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaGoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartaGo/cartaGo_backend/CartaGoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartaGo.cartaGo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaGoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
